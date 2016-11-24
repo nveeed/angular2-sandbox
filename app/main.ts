f1aa8c1d7da9085e2b65c9685eeba6e6b2aa7cad
Naveed Hassan <naveed.hassan@nxb.com.pk>
2016-11-24 15:30:02 +0500

    cleanup

--- app/main.ts
+++ app/main.ts
@@ -2,4 +2,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
+//noinspection TypeScriptValidateTypes
 platformBrowserDynamic().bootstrapModule(AppModule);
