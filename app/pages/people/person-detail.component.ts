b432f3f2795f5400c1476915f05b31da524c97ab
Naveed Hassan <naveed.hassan@nxb.com.pk>
2016-12-06 16:39:29 +0500

    router: imparetive navigation

--- app/pages/people/person-detail.component.ts
+++ app/pages/people/person-detail.component.ts
@@ -9,14 +9,23 @@ import {PeopleService} from "./people.service";
     template: `
     <h3 *ngIf="person">{{person.id}}: {{person.name}}</h3>
     <back-btn></back-btn>
+    <button *ngIf="person && person.id !== 1" (click)="goToDMDetails()">See DM details</button>
     `,
 })
 export class PersonDetailComponent implements OnInit{
     person: Person;
 
-    constructor(private peopleService: PeopleService, private route: ActivatedRoute){}
+    constructor(
+        private peopleService: PeopleService,
+        private route: ActivatedRoute,
+        private router: Router
+    ){}
 
     ngOnInit(){
         this.route.params.subscribe(params => this.person = this.peopleService.getPerson(+params['id']));
+    }
+
+    goToDMDetails(): void {
+        this.router.navigate(['/people/person', 1]);
     }
 }
